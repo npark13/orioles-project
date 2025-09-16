c16cf2e8f958680e0c8dfeea788972cb51d1548b
Nancy Park <nancypark@MacBook-Air-2.local>
2025-09-15 23:23:39 -0400

    updated csv files

---
+++ visuals/plots.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def readdata(filename):
+
+    with open(filename, "r") as f:
+        return "
+    
+def summarizeData(hf):
+
+    innings = []
+    years = []
+    homeruns = []
+
+
