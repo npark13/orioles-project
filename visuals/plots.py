cabcfe029360ec919553fe336d554bce4088ca98
Nancy Park <nancypark@MacBook-Air-2.local>
2025-09-16 22:23:48 -0400

    began plot code and moved old data to dummy file

--- visuals/plots.py
+++ visuals/plots.py
@@ -4,12 +4,30 @@ import matplotlib.pyplot as plt
 def readdata(filename):
 
     with open(filename, "r") as f:
-        return "
+        return np.genfromtxt(f, delimiter=',', skip_header=1; unpack=False)
     
-def summarizeData(hf):
+def summarizeData(plays_csv="out/plays.csv"):
 
     innings = []
     years = []
     homeruns = []
 
+    for each_row_of_data in dataStats:
+        innings.append(each_row_of_data[1])
+        
 
+    plt.plot(innings, runs, "o")
+    plt.title("Scores by Inning")
+    plt.xlabel("Year", fontsize=14)
+    plt.ylabel("Scoring", fontsize=14)
+
+    plt.show()
+        
+
+
+def HFregression(hf):
+    plt.title("Regression Across Innings", fontsize=14)
+    plt.xlabel("Innings", fontsize=14)
+    plt.ylabel("Average Scorings", fontsize=14)
+    plt.axhline(y=0, color='black', linestyle='-')
+    plt.plot()
